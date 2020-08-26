13c6b15e516b86365296b671caac332654795def
quelsh <quelsh2020@gmail.com>
2020-08-26 08:18:28 -0700

    GX-15 Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,5 @@ echo $IMAGE
 docker ps --filter "name=golang-server"
 docker pull $IMAGE
 docker rm -f golang-server || true
-docker run -d \
-           --name golang-server \
-           -p 8080:8080 \
-           $IMAGE
+docker run -d --name golang-server -p 8080:8080 $IMAGE
 docker ps --filter "name=golang-server"
